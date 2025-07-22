6461a4f4b5e2176713e39905506eccc2130e670b
David Mokos <davidmokos@gmail.com>
2025-07-22 12:08:50 +0200

    adds a component library

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Stack } from "expo-router";
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "../global.css";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <GluestackUIProvider mode="light">
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </GluestackUIProvider>
   );
 }
