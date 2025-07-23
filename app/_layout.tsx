a4727c64760d0c6119cb3b2805f0ce4bee7671a8
David Mokos <davidmokos@gmail.com>
2025-07-23 17:09:10 +0200

    nativewind template

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,30 +2,25 @@ import "../global.css";
 
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import { PortalHost } from "@rn-primitives/portal";
-import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { StatusBar } from "expo-status-bar";
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-// import { KeyboardProvider } from "react-native-keyboard-controller";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { KeyboardProvider } from "react-native-keyboard-controller";
 import { ThemeProvider as NavigationTheme } from "@react-navigation/native";
 import ThemeProvider from "@/providers/ThemeProvider";
 
 import { useColorScheme, useInitialAndroidBarSync } from "@/lib/useColorScheme";
 import { NAV_THEME } from "@/theme";
 import { Stack } from "expo-router";
-import { useEffect } from "react";
 
-// export {
-//   // Catch any errors thrown by the Layout component.
-//   ErrorBoundary,
-// } from "expo-router";
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
 
 export default function RootLayout() {
   useInitialAndroidBarSync();
   const { colorScheme, isDarkColorScheme } = useColorScheme();
-
-  // const { colorScheme, isDarkColorScheme } = { colorScheme: "light", isDarkColorScheme: false };
-
-  console.log("colorScheme", colorScheme, isDarkColorScheme);
 
   return (
     <>
@@ -35,18 +30,18 @@ export default function RootLayout() {
       />
 
       <ActionSheetProvider>
-        {/* <KeyboardProvider statusBarTranslucent navigationBarTranslucent> */}
-        <GestureHandlerRootView style={{ flex: 1 }}>
-          <BottomSheetModalProvider>
-            <ThemeProvider>
-              <NavigationTheme value={NAV_THEME[colorScheme]}>
-                <RootNavigator />
-                <PortalHost />
-              </NavigationTheme>
-            </ThemeProvider>
-          </BottomSheetModalProvider>
-        </GestureHandlerRootView>
-        {/* </KeyboardProvider> */}
+        <KeyboardProvider statusBarTranslucent navigationBarTranslucent>
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <BottomSheetModalProvider>
+              <ThemeProvider>
+                <NavigationTheme value={NAV_THEME[colorScheme]}>
+                  <RootNavigator />
+                  <PortalHost />
+                </NavigationTheme>
+              </ThemeProvider>
+            </BottomSheetModalProvider>
+          </GestureHandlerRootView>
+        </KeyboardProvider>
       </ActionSheetProvider>
     </>
   );
