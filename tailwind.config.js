357297ca8aee3f70288a19eb1b332b12338bb623
David Mokos <davidmokos@gmail.com>
2025-08-26 12:18:33 +0200

    simplifies the template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,9 @@
-const { hairlineWidth } = require('nativewind/theme');
-const { colors } = require('./theme/colors');
-
-const withOpacity = name => `rgb(var(--${name}) / <alpha-value>)`;
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
-  presets: [require('nativewind/preset')],
-  darkMode: 'class',
-
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
-    extend: {
-      colors: {
-        border:      withOpacity('border'),
-        input:       withOpacity('input'),
-        ring:        withOpacity('ring'),
-        background:  withOpacity('background'),
-        foreground:  withOpacity('foreground'),
-        primary:   { DEFAULT: withOpacity('primary'),   foreground: withOpacity('primary-foreground') },
-        secondary: { DEFAULT: withOpacity('secondary'), foreground: withOpacity('secondary-foreground') },
-        destructive:{DEFAULT: withOpacity('destructive'),foreground: withOpacity('destructive-foreground')},
-        muted:     { DEFAULT: withOpacity('muted'),     foreground: withOpacity('muted-foreground') },
-        accent:    { DEFAULT: withOpacity('accent'),    foreground: withOpacity('accent-foreground') },
-        popover:   { DEFAULT: withOpacity('popover'),   foreground: withOpacity('popover-foreground') },
-        card:      { DEFAULT: withOpacity('card'),      foreground: withOpacity('card-foreground') },
-      },
-      borderWidth: { hairline: hairlineWidth() },
-    },
+    extend: {},
   },
-
-  plugins: [
-    ({ addBase }) => {
-      addBase({
-        ':root': colors.light,
-        '.dark': colors.dark,
-      });
-    },
-  ],
+  plugins: [],
 };
--- theme/index.ts
+++
@@ -1,3 +0,0 @@
-export { NAV_THEME } from './navigation';
-export { colors } from './colors';
-export type { Scheme } from './colors';
