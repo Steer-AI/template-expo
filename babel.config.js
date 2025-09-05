7b42b95fd2e7dd8cf3566fba0b10fa67780ff2a6
David Mokos <davidmokos@gmail.com>
2025-09-05 20:32:55 +0200

    Updates the template

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,13 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      [
+        "babel-preset-expo",
+        {
+          jsxImportSource: "nativewind",
+          unstable_transformImportMeta: true, // to make zustand work on web
+        },
+      ],
       "nativewind/babel",
     ],
   };
